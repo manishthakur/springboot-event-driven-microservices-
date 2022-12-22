73478b0d30cc672215fa5a429793a0d38936d790
Manish Thakur <manishhakur.in@hotmail.com>
2022-12-22 14:45:00 -0500

    first commit

---
+++ base-domains/src/main/java/net/javaguides/basedomains/dto/OrderEvent.java
@@ -0,0 +1,17 @@
+package net.javaguides.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String message;
+    private String status;
+    private Order order;
+    private double price;
+
+}
